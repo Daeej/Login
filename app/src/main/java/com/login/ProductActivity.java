27c7ce21838a1a4cf173509a6e515056cd07df10
AbdulNY <abdullahdaeej@gmail.com>
2016-04-14 17:51:38 -0400

    Mike comitt

--- app/src/main/java/com/login/ProductActivity.java
+++ app/src/main/java/com/login/ProductActivity.java
@@ -8,6 +8,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
